27ab3bb6f03ad431d8838c800bfd58a436353a30
kukushkinace1 <122355565+kukushkinace1@users.noreply.github.com>
2024-01-15 20:09:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+from web3 import Web3
+import random
+import time
+from loguru import logger
+from sys import stderr
+from web3.exceptions import TimeExhausted
+
+# Определение диапазона суммы перевода и времени задержки
+min_transfer = 69  # минимальная сумма перевода в МЕМЕ
+max_transfer = 75  # максимальная сумма перевода в МЕМЕ
+min_delay = 25  # минимальное время задержки в секундах
+max_delay = 35  # максимальное время задержки в секундах
+GWEI_CONTROL = 20
+
+logger.remove()
+logger.add(stderr, format="<white>{time:HH:mm:ss}</white> | <level>{level: <3}</level> | <level>{message}</level>")
+
+web3 = Web3(Web3.HTTPProvider('https://ethereum.publicnode.com'))
+
+# Чтение приватных ключей
+with open('private_keys.txt', 'r') as file:
+    private_keys = file.read().splitlines()
+
+MEME_abi = '[{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"uint256","name":"totalSupply_","type":"uint256"},{"internalType":"address","name":"treasury","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"InvalidShortString","type":"error"},{"inputs":[{"internalType":"string","name":"str","type":"string"}],"name":"StringTooLong","type":"error"},{"inputs":[],"name":"Unauthorized","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[],"name":"EIP712DomainChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"tokenPool","type":"address"}],"name":"TokenPoolUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"eip712Domain","outputs":[{"internalType":"bytes1","name":"fields","type":"bytes1"},{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"version","type":"string"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"verifyingContract","type":"address"},{"internalType":"bytes32","name":"salt","type":"bytes32"},{"internalType":"uint256[]","name":"extensions","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_tokenPool","type":"address"}],"name":"setTokenPool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokenPool","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+MEME_address = '0xb131f4a55907b10d1f0a50d8ab8fa09ec342cd74'
+
+
+def cheker_gwei():
+    max_gwei = GWEI_CONTROL * 10 ** 9
+    if web3.eth.gas_price > max_gwei:
+        logger.info('Газ большой, пойду спать')
+        while web3.eth.gas_price > max_gwei:
+            time.sleep(60)
+        logger.info('Газ в норме. Продолжаю работу')
+
+
+from_address = web3.eth.account.from_key(private_keys[0]).address
+balance = web3.eth.get_balance(from_address)
+
+with open('addresses.txt', 'r') as file:
+    wallets = file.read().splitlines()
+
+MEME_contract = web3.eth.contract(address=web3.to_checksum_address(MEME_address), abi=MEME_abi)
+
+for wallet in wallets:
+    cheker_gwei()
+
+    amount_to_send = web3.to_wei(random.uniform(min_transfer, max_transfer), 'ether')
+    try:
+        # Подготовьте данные для транзакции
+        tx = MEME_contract.functions.transfer(wallet, amount_to_send)
+        tx = tx.build_transaction({
+            'from': from_address,
+            'chainId': web3.eth.chain_id,
+            'gasPrice': int(web3.eth.gas_price*1.1),
+            'nonce': web3.eth.get_transaction_count(from_address),
+        })
+        gasLimit = web3.eth.estimate_gas(tx)
+        tx['gas'] = int(gasLimit*1.2)
+
+        signed_tx = web3.eth.account.sign_transaction(tx, private_keys[0])
+        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        status = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=360).status
+        if status == 1:
+            logger.info(f"send to {wallet} : {amount_to_send / 10 ** 18} https://etherscan.io/tx/{tx_hash.hex()}")
+        else:
+            logger.error(f'[{account.address}] transaction failed!')
+    except Exception as err:
+        print(err)
+
+    time.sleep(random.randint(min_delay, max_delay))
+
+
